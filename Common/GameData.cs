0d7d7b14afb88bfcff1c9398bfb4da547814fc75
nightSanches <galesavich@gmail.com>
2024-11-30 19:07:10 +0500

    Создание класса GameData. Савичев

---
+++ Common/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class GameData
+    {
+        public ViewModelGames Player1Data { get; set; }
+        public List<ViewModelGames> OtherPlayersData { get; set; }
+    }
+}
